5f7ebf5820a74b4c42434e1d9c23b4215fcd38e0
Tim Mitra <tim@it-guy.com>
2024-03-08 15:52:56 -0500

    handle and verify the purchase

--- BuildingATipJar/TipStore.swift
+++ BuildingATipJar/TipStore.swift
@@ -8,6 +8,12 @@
 import Foundation
 import StoreKit
 
+enum TipsError: Error {
+  case failedVerification
+}
+
+typealias PurchaseResult = Product.PurchaseResult
+
 final class TipStore: ObservableObject {
   
   @Published private(set) var items = [Product]()
@@ -15,6 +21,20 @@ final class TipStore: ObservableObject {
   init() {
     Task { [weak self] in
       await self?.retrieveProducts()
+    }
+  }
+  
+  func purchase(_ item: Product) async {
+    
+    do {
+      
+      let result = try await item.purchase()
+      
+      try await handlePurchase(from: result)
+      
+    } catch {
+      // TODO: Handle error
+      print(error)
     }
   }
 }
@@ -29,6 +49,42 @@ private extension TipStore {
     } catch {
       // TODO: Handle Error
       print(error)
+    }
+  }
+  
+  func handlePurchase(from result: PurchaseResult) async throws {
+    
+    switch result {
+      
+    case .success(let verification):
+      print("Purchase was a success, now it's time to verify their purchase.")
+      
+      // verify
+      let transaction = try checkVerified(verification)
+      
+      // TODO: Verification was Ok, update UI
+      
+      await transaction.finish()
+      
+    case .pending:
+      print("The user needs to complete some action on their account before they can complete the purchase.")
+      
+    case .userCancelled:
+      print("The user hit cancel before their transaction started.")
+      
+    default:
+      break
+    }
+  }
+  
+  func checkVerified<T>(_ result: VerificationResult<T>) throws -> T {
+    
+    switch result {
+    case .unverified:
+      print("The verification of the user failed.")
+      throw TipsError.failedVerification
+    case .verified(let safe):
+      return safe
     }
   }
 }
