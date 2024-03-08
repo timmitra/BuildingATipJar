ac5cd76529b51622b58b7bdf020f8115fbd024fd
Tim Mitra <tim@it-guy.com>
2024-03-08 11:03:56 -0500

    Initial Commit

---
+++ BuildingATipJar/BuildingATipJarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingATipJarApp.swift
+//  BuildingATipJar
+//
+//  Created by Tim Mitra on 2024-03-08.
+//
+
+import SwiftUI
+
+@main
+struct BuildingATipJarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuildingATipJar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BuildingATipJar
+//
+//  Created by Tim Mitra on 2024-03-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
